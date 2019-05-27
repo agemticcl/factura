2c9241ab70ec7205b1de6886a7055492bf9a771f
dansanti <dansanti@gmail.com>
2019-05-27 06:35:36 -0400

    [FIX]Obtener tasa IVA
    Debe ser retornada

--- models/consumo_folios.py
+++ models/consumo_folios.py
@@ -409,9 +409,12 @@ version="1.0">
                     MntExe += l.credit
                 else:
                     MntExe += l.debit
-        TasaIVA = self.env['account.move.line'].search([('move_id', '=', rec.id), ('tax_line_id.amount', '>', 0)], limit=1).tax_line_id.amount
         MntTotal = Neto + MntExe + TaxMnt
-        return Neto, MntExe, TaxMnt, MntTotal
+        TasaIVA = self.env['account.move.line'].search([
+                ('move_id', '=', rec.id), 
+                ('tax_line_id.amount', '>', 0)
+            ], limit=1).tax_line_id.amount
+        return Neto, MntExe, TaxMnt, MntTotal, TasaIVA
 
     def getResumen(self, rec):
         det = collections.OrderedDict()
@@ -423,7 +426,7 @@ version="1.0">
         if rec.canceled:
             det['Anulado'] = 'A'
             return det
-        Neto, MntExe, TaxMnt, MntTotal = self._get_totales(rec)
+        Neto, MntExe, TaxMnt, MntTotal, TasaIVA = self._get_totales(rec)
         if MntExe > 0 :
             det['MntExe'] = self.currency_id.round(MntExe)
         if TaxMnt > 0:
